dc708a0adde0f6ca5c785f794cb2646ed05651c8
Benjamin Chan <chanb@ohsu.edu>
2017-05-19 14:32:04 -0700

    Start coding

---
+++ scripts/script.R
@@ -0,0 +1,29 @@
+setwd("~/Projects/NewProject/scripts")
+
+library(checkpoint)
+checkpoint("2017-01-01", use.knitr = TRUE)
+
+Sys.time0 <- Sys.time()
+
+sink("script.log")
+files <- c("header.yaml",
+           "preamble.Rmd")
+f <- file("master.Rmd", open = "w")
+for (i in 1:length(files)) {
+    x <- readLines(files[i])
+    writeLines(x, f)
+}
+close(f)
+library(knitr)
+library(rmarkdown)
+opts_chunk$set(fig.path = "../figures/", dpi = 300)
+knit("master.Rmd", output = "../docs/index.md")
+# pandoc("../docs/index.md", format = "html")
+file.remove("master.Rmd")
+sink()
+
+sink("session.log")
+list(completionDateTime = Sys.time(),
+     executionTime = Sys.time() - Sys.time0,
+     sessionInfo = sessionInfo())
+sink()
---
+++ scripts/script.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Microsoft\MRO-3.3.2\bin\x64\Rscript.exe" "C:\Users\chanb\Documents\Projects\NewProject\scripts\script.R"
