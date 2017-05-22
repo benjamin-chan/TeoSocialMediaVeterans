7368390e35f859cb14cbacaebcffd59b2eb8d4bd
Benjamin Chan <chanb@ohsu.edu>
2017-05-22 14:46:21 -0700

    Create plotRates function

---
+++ lib/plotRates.R
@@ -0,0 +1,21 @@
+plotRates <- function (y, title) {
+  require(ggplot2)
+  theme_set(theme_bw())
+  require(svglite)
+  G <- 
+    df %>% 
+    ggplot +
+    aes(x = date, y = get(y)) +
+    facet_grid(adText ~ adImage) +
+    geom_point() +
+    geom_smooth() +
+    scale_x_date("Date") +
+    scale_y_continuous("Count") +
+    labs(title = title) +
+    theme(legend.position = "bottom",
+          panel.grid.minor = element_blank(),
+          plot.title = element_text(hjust = 0.5))
+  ggsave(paste0("../figures/", y, ".png"), dpi = 300)
+  ggsave(paste0("../figures/", y, ".svg"), dpi = 300)
+  file.info(paste0("../figures/", grep(y, list.files("../figures/"), value = TRUE)))[c("size", "mtime")]
+}
