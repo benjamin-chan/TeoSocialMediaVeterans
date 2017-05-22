f437ce3caa5ca32935db1471f9c07e7b2d553995
Benjamin Chan <chanb@ohsu.edu>
2017-05-22 14:47:18 -0700

    Fix Windows batch script

--- scripts/script.bat
+++ scripts/script.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Microsoft\MRO-3.3.2\bin\x64\Rscript.exe" "C:\Users\chanb\Documents\Projects\NewProject\scripts\script.R"
+"C:\Program Files\R\R-3.3.3\bin\x64\Rscript.exe" "C:\Users\chanb\Documents\Projects\TeoSocialMediaVeterans\scripts\script.R"
