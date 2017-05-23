1e0ccdb1e3a05b69b3de6e2237cd282ea317fb16
Benjamin Chan <chanb@ohsu.edu>
2017-05-23 11:04:12 -0700

    Set up framework of modeling script

--- scripts/script.R
+++ scripts/script.R
@@ -10,7 +10,8 @@ files <- c("header.yaml",
            "preamble.Rmd",
            "readData.Rmd",
            "plotData.Rmd",
-           "corrData.Rmd")
+           "corrData.Rmd",
+           "modelCounts.Rmd")
 f <- file("master.Rmd", open = "w")
 for (i in 1:length(files)) {
     x <- readLines(files[i])
