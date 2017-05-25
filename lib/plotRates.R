279af2faa477c8d01c7d7b6c58022779903156ba
Benjamin Chan <chanb@ohsu.edu>
2017-05-25 10:59:34 -0700

    Edit plotting functions

--- lib/plotRates.R
+++ lib/plotRates.R
@@ -1,4 +1,5 @@
 plotRates <- function (y, x, filename, title) {
+  require(tools)
   require(ggplot2)
   theme_set(theme_bw())
   require(svglite)
@@ -8,14 +9,14 @@ plotRates <- function (y, x, filename, title) {
     aes(x = get(x), y = get(y)) +
     facet_grid(adText ~ adImage) +
     geom_point(alpha = 1/2) +
-    geom_smooth() +
-    scale_x_log10(x) +
+    geom_smooth(method = "glm", method.args = list(family = "poisson")) +
+    scale_x_log10(toTitleCase(x)) +
     scale_y_continuous("Count") +
     labs(title = title) +
     theme(legend.position = "bottom",
           panel.grid.minor = element_blank(),
           plot.title = element_text(hjust = 0.5))
-  ggsave(paste0("../figures/", y, ".png"), dpi = 300)
-  ggsave(paste0("../figures/", y, ".svg"), dpi = 300)
+  ggsave(paste0("../figures/", filename, ".png"), dpi = 300)
+  ggsave(paste0("../figures/", filename, ".svg"), dpi = 300)
   file.info(paste0("../figures/", grep(filename, list.files("../figures/"), value = TRUE)))[c("size", "mtime")]
 }
--- lib/plotResid.R
+++ lib/plotResid.R
@@ -1,21 +1,21 @@
-plotResid <- function (y, title) {
+plotResid <- function (y, x, filename, title) {
+  require(tools)
   require(ggplot2)
   theme_set(theme_bw())
   require(svglite)
   G <- 
     df %>% 
     ggplot +
-    aes(x = date, y = get(y)) +
+    aes(x = get(x), y = get(y)) +
     facet_grid(adText ~ adImage) +
     geom_point(alpha = 1/2) +
-    geom_smooth() +
-    scale_x_date("Date") +
+    scale_x_log10(toTitleCase(x)) +
     scale_y_continuous("Residual count") +
     labs(title = title) +
     theme(legend.position = "bottom",
           panel.grid.minor = element_blank(),
           plot.title = element_text(hjust = 0.5))
-  ggsave(paste0("../figures/", y, ".png"), dpi = 300)
-  ggsave(paste0("../figures/", y, ".svg"), dpi = 300)
-  file.info(paste0("../figures/", grep(y, list.files("../figures/"), value = TRUE)))[c("size", "mtime")]
+  ggsave(paste0("../figures/", filename, ".png"), dpi = 300)
+  ggsave(paste0("../figures/", filename, ".svg"), dpi = 300)
+  file.info(paste0("../figures/", grep(filename, list.files("../figures/"), value = TRUE)))[c("size", "mtime")]
 }
