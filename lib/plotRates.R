cca89cf8aa9b649d75d5801651de64fe4c0fcdf9
Benjamin Chan <benjamin.ks.chan@gmail.com>
2017-05-24 21:37:45 -0700

    Generalize plotRates function for any x variable

--- lib/plotRates.R
+++ lib/plotRates.R
@@ -1,15 +1,15 @@
-plotRates <- function (y, title) {
+plotRates <- function (y, x, filename, title) {
   require(ggplot2)
   theme_set(theme_bw())
   require(svglite)
   G <- 
     df %>% 
     ggplot +
-    aes(x = date, y = get(y)) +
+    aes(x = get(x), y = get(y)) +
     facet_grid(adText ~ adImage) +
     geom_point(alpha = 1/2) +
     geom_smooth() +
-    scale_x_date("Date") +
+    scale_x_continuous(x) +
     scale_y_continuous("Count") +
     labs(title = title) +
     theme(legend.position = "bottom",
@@ -17,5 +17,5 @@ plotRates <- function (y, title) {
           plot.title = element_text(hjust = 0.5))
   ggsave(paste0("../figures/", y, ".png"), dpi = 300)
   ggsave(paste0("../figures/", y, ".svg"), dpi = 300)
-  file.info(paste0("../figures/", grep(y, list.files("../figures/"), value = TRUE)))[c("size", "mtime")]
+  file.info(paste0("../figures/", grep(filename, list.files("../figures/"), value = TRUE)))[c("size", "mtime")]
 }
